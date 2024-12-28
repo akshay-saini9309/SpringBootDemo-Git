530f73e999489d3b8e85d4871b2b387b17f16866
akshay-saini9309 <akshaysini9309@gmail.com>
2024-12-28 14:53:50 +0530

    added demoforgit project

---
+++ demoforgit/src/test/java/com/example/git/demoforgit/DemoforgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git.demoforgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoforgitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
